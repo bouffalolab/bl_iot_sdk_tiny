75df6e87ffa10b905c9dbb0efc5fd35f96955c55
wyhong <wyhong@bouffalolab.com>
2024-12-31 15:19:30 +0800

    bouffalolab_release_bl_iot_sdk_1.6.40-2087-g5cef46e51

--- scripts/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "This script was used to install and update toolchain before."
-echo "Now, it is going to stop maintanance."
-echo "Please try third_party/bouffalolab/env-setup.sh to install/update toolchain under your matter repo."
